420b3b984a77c9a5a98294d87cd782d94a485632
andreiosg <andrej.gardijan@gmail.com>
2019-12-25 15:55:44 +0100

    Solved day 6 part 2.

---
+++ 6-2.py
@@ -0,0 +1,26 @@
+def traverse(path, orbits, orbitee):
+    if orbitee not in orbits:
+        return 
+    
+    path.append(orbitee)
+    traverse(path, orbits, orbits[orbitee])
+
+# works because all orbit paths lead to the center of mass
+def dist2santa(orbits):
+    you = []
+    traverse(you, orbits, orbits['YOU'])
+    santa = []
+    traverse(santa, orbits, orbits['SAN'])
+
+    for planet in you:
+        if planet in santa:
+            return you.index(planet) + santa.index(planet)
+
+
+orbits = {}
+with open('data/6.txt') as f:
+    for line in f.readlines():
+        orbit = line.strip().split(')')
+        orbits[orbit[1]] = orbit[0]
+
+print(dist2santa(orbits))
