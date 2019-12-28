1eed1f30cdd3270c19fa0d02934365b1d1dbb706
andreiosg <andrej.gardijan@gmail.com>
2019-12-28 23:23:26 +0100

    Solved day 4 part 1.

---
+++ 4-1.py
@@ -0,0 +1,25 @@
+def count_matching(lo, hi):
+    cnt = 0
+    for n in range(lo, hi):
+        inc = 0
+        while n // 10 > 0:
+            ones = n % 10
+            n //= 10
+            tens = n % 10
+            if ones == tens:
+                inc = 1
+            if ones < tens:
+                inc = 0
+                break
+        cnt += inc
+
+    return cnt
+
+
+with open('data/4.txt') as f:
+    interval = tuple(int(n) for n in f.readline().split('-'))
+
+print(count_matching(*interval))
+
+
+
