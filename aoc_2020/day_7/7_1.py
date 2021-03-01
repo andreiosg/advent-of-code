2bfb4830b383844be9e1c3581ae0518c74e0fdd1
andreiosg <andrej.gardijan@tutanota.com>
2021-03-01 20:53:30 +0100

    AoC 2020 day 7.

---
+++ aoc_2020/day_7/7_1.py
@@ -0,0 +1,32 @@
+import re
+
+def hasShinyGold(bags: dict, key: str) -> bool:
+    # shiny gold was found
+    if 'shiny gold' in bags[key]: return True
+    # innermost bag
+    if not len(bags[key]): return False
+    
+    innersContain = False
+    for bag in bags[key]:
+        innersContain = innersContain or hasShinyGold(bags, bag)
+
+    return innersContain
+
+with open('in.txt') as f:
+    description = f.readlines()
+
+bags = {}
+for line in description:
+    # the first 2 words represent the key bag
+    key = ' '.join(line.split()[:2])
+    bags[key] = []
+    # all the bags the key bag contains, single digit bag multiplier
+    items = re.findall(r'[1-9] [a-z]+ [a-z]+', line)
+    for item in items:
+        n = int(item[0])
+        innerBag = item[2:]
+        # unnecessarily complex, multiple bag instances don't affect part 1
+        # bags[key].append({'name': innerBag, 'n': n}) 
+        bags[key].append(innerBag)
+
+print(sum(hasShinyGold(bags, key) for key in bags))
---
+++ aoc_2020/day_7/7_2.py
@@ -0,0 +1,30 @@
+import re
+
+def cumulativeBags(bags: dict, key: str) -> bool:
+    # innermost bag
+    if not len(bags[key]): return 1
+    
+    # itself + all the others it contains, recursively
+    n = 1
+    for bag in bags[key]:
+        n += bag['n'] * cumulativeBags(bags, bag['name'])
+
+    return n
+
+with open('in.txt') as f:
+    description = f.readlines()
+
+bags = {}
+for line in description:
+    # the first 2 words represent the key bag
+    key = ' '.join(line.split()[:2])
+    bags[key] = []
+    # all the bags the key bag contains, single digit bag multiplier
+    items = re.findall(r'[1-9] [a-z]+ [a-z]+', line)
+    for item in items:
+        n = int(item[0])
+        innerBag = item[2:]
+        bags[key].append({'name': innerBag, 'n': n}) 
+
+# -1 since we don't count shiny gold itself
+print(cumulativeBags(bags, 'shiny gold') - 1)
