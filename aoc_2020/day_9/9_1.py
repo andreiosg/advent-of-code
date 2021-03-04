bf2cf2a2a92bf8880dcc74586d794f6bdb41667f
andreiosg <andrej.gardijan@tutanota.com>
2021-03-04 03:51:09 +0100

    AoC 2020 day 9 part 1.

---
+++ aoc_2020/day_9/9_1.py
@@ -0,0 +1,15 @@
+def isSumNC2(preamble: list, s: int) -> bool:
+    nums = set(preamble)     
+    for num in preamble: 
+        if s-num in nums: return True
+    return False
+
+def findInvalid(cypher: str, n: int) -> int:
+    for i, num in enumerate(cypher[n:]):
+        if isSumNC2(cypher[i:i+n], num): continue
+        return num
+
+with open('in.txt') as f:
+    cypher = list(map(int, f.read().split()))
+
+print(findInvalid(cypher, 25))
