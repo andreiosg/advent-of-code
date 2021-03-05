a4ccd50a49e33402e31118276a8d44753d1bca73
andreiosg <andrej.gardijan@tutanota.com>
2021-03-06 00:31:57 +0100

    Algorithm description.

--- aoc_2020/day_10/10_2.py
+++ aoc_2020/day_10/10_2.py
@@ -1,9 +1,11 @@
+# counting the permutations boils down to counting all the leaves of the graph
+# dfs with memoization
 def countPermutations(joltageBranches: list, joltage: int, memo: dict = {}) -> int:
     if joltage in memo:
         return memo[joltage]
     elif joltageBranches[joltage]:
         memo[joltage] = sum(countPermutations(joltageBranches, j, memo) 
-                       for j in joltageBranches[joltage])
+                            for j in joltageBranches[joltage])
         return memo[joltage]
 
     return 1
@@ -16,6 +18,7 @@ joltages.extend(init.values())
 joltages.sort()
 
 maxStep = 3
+# join every valid branch joltage (step=3) to each joltage
 joltageBranches = {joltage: [joltages[i+j] for j in range(1, maxStep+1)
                    if i+j < len(joltages) and joltages[i+j]-joltage <= maxStep]
                    for i, joltage in enumerate(joltages)}
