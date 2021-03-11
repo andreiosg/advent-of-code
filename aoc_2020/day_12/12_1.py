3b9308a633bdda80eaef61569ec0c71bedf9494f
andreiosg <andrej.gardijan@tutanota.com>
2021-03-11 01:38:39 +0100

    AoC 2020 day 12 part 1.

---
+++ aoc_2020/day_12/12_1.py
@@ -0,0 +1,34 @@
+def navigate(instructions: list) -> int:
+    # ship normal multipliers, represents E, N, W, S respectively
+    muls = ((1, 0),
+            (0, 1),
+            (-1, 0),
+            (0, -1))
+
+    ship = {'x': 0,
+            'y': 0,
+            'dir': muls[0]}
+
+    for action, val in instructions:
+        if action == 'N':
+            ship['y'] += val
+        elif action == 'S':
+            ship['y'] -= val
+        elif action == 'E':
+            ship['x'] += val
+        elif action == 'W':
+            ship['x'] -= val
+        elif action in ['L', 'R']:
+            turn = 90
+            sign = -1 if action == 'R' else 1
+            ship['dir'] = muls[(muls.index(ship['dir'])+sign*int(val/turn))%len(muls)]
+        elif action == 'F':
+            ship['x'] += ship['dir'][0] * val
+            ship['y'] += ship['dir'][1] * val
+
+    return abs(ship['x']) + abs(ship['y'])
+
+with open('in.txt') as f:
+    instructions = [(line[0], int(line[1:-1])) for line in f]
+
+print(navigate(instructions))
